9e01984b87dcbd21c790008eb0c0e6b93a5bce04
OuKai <t110ac1013@ntut.org.tw>
2022-04-08 09:33:12 +0800

    Add branch1
    Add food

--- homework4.cs
+++ homework4.cs
@@ -1,7 +1,7 @@
 string food = "";
 
 Random random = new Random();
-int i = random.Next(0, 3);
+int i = random.Next(0, 8);
 
 switch (i)
 {
@@ -14,5 +14,23 @@ switch (i)
     case 2:
         food = "Spicy Dumplings";
         break;
+    case 3:
+        food = "Beef";
+        break;
+    case 4:
+        food = "Sausage";
+        break;
+    case 5:
+        food = "Pizza";
+        break;  
+    case 6
+        food = "Spaghetti";
+        break;
+    case 7:
+        food = "Stinky tofu";
+        break;
+    case 8:
+        food = "Braised pork on rice";
+        break; 
 }
 Console.WriteLine("Random result : " + food + ".");
